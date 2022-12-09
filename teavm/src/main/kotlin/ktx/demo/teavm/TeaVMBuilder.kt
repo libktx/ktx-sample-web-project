06302c69a1676cbac0b1c9f41e99ac21c48bbd0d
czyzby <czyzby@users.noreply.github.com>
2022-12-09 22:41:48 +0000

    Automatic deployment: KTX 1.11.0-rc3, gdx-liftoff 1.11.0.5.

---
+++ teavm/src/main/kotlin/ktx/demo/teavm/TeaVMBuilder.kt
@@ -0,0 +1,29 @@
+package ktx.demo.teavm
+
+import java.io.File
+import com.github.xpenatan.gdx.backends.teavm.TeaBuildConfiguration
+import com.github.xpenatan.gdx.backends.teavm.TeaBuilder
+import com.github.xpenatan.gdx.backends.teavm.plugins.TeaReflectionSupplier
+import com.github.xpenatan.gdx.backends.web.gen.SkipClass
+
+/** Builds the TeaVM/HTML application. */
+@SkipClass
+object TeaVMBuilder {
+  @JvmStatic fun main(arguments: Array<String>) {
+    val teaBuildConfiguration = TeaBuildConfiguration().apply {
+      assetsPath.add(File("../assets"))
+      webappPath = File("build/dist").canonicalPath
+      // You can switch this setting during development:
+      obfuscate = true
+      // Register any extra classpath assets here:
+      // additionalAssetsClasspathFiles += "ktx/demo/asset.extension"
+    }
+
+    // Register any classes or packages that require reflection here:
+    // TeaReflectionSupplier.addReflectionClass("ktx.demo.reflect")
+
+    val tool = TeaBuilder.config(teaBuildConfiguration)
+    tool.mainClass = "ktx.demo.teavm.TeaVMLauncher"
+    TeaBuilder.build(tool)
+  }
+}
---
+++ teavm/src/main/kotlin/ktx/demo/teavm/TeaVMLauncher.kt
@@ -0,0 +1,16 @@
+@file:JvmName("TeaVMLauncher")
+
+package ktx.demo.teavm
+
+import com.github.xpenatan.gdx.backends.teavm.TeaApplicationConfiguration
+import com.github.xpenatan.gdx.backends.web.WebApplication
+import ktx.demo.Main
+
+/** Launches the TeaVM/HTML application. */
+fun main() {
+  val config = TeaApplicationConfiguration("canvas").apply {
+    width = 640
+    height = 480
+  }
+  WebApplication(Main(), config)
+}
